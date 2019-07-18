a7ad2fdc645a934d043c488ca5d3c1132ff6835b
C1one_38 <c1one@yandex.ru>
2019-07-18 18:43:50 +0300

    fix api call

--- shared/api.js
+++ shared/api.js
@@ -1,6 +1,6 @@
 import fetch from "unfetch";
 
-export const sendMail = async (phone, name) => {
+export const sendMail = async (phone, name = "") => {
   await fetch(
     `https://eu-gb.functions.cloud.ibm.com/api/v1/web/c1one@yandex.ru_dev/visatour-komi/mail.json?phone=${phone}&name=${name}`
   );
